44db5dfb7981ead69a498e42b0579b83be6b21e8
emily <emi222ly@gmail.com>
2023-09-26 12:21:05 -0300

    2.1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,8 +29,11 @@
             </div>
         </nav>
         <!-- Header-->
+        <div class="container px-4 px-lg-5 mt-3">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center pb-3"><input type="text" name="" id="" placeholder="Pesquisar" class="input-group input-text form-control"></div>
+            </div>
         <header class="bg-dark py-5">
-            <div class="container px-4 px-lg-5 my-5">
+            <div class="container px-5 px-lg-5 my-0">
                 <div class="text-center text-white">
                     <h1 class="display-4 fw-bolder">Livros disponíveis</h1>
                 </div>
@@ -38,15 +41,14 @@
         </header>
         <!-- Section-->
         <section class="py-1">
-            <div class="container px-4 px-lg-5 mt-5">
-                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center pb-5"><input type="text" name="" id="" placeholder="Pesquisar" class="input-group input-text form-control"></div>
+            <div class="container px-4 px-lg-5 mt-3">
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                    <div class="col mb-5">
+                    <div class="col mb-5 w-auto">
                         <div class="card h-100">
                             <!-- Product image-->
-                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <img class="card-img-top" src="{{asset('200x300.jpg')}}" alt="" />
                             <!-- Product details-->
-                            <div class="card-body p-4">
+                            <div class="card-body p-2">
                                 <div class="text-center">
                                     <!-- Product name-->
                                     <h5 class="fw-bolder">Título</h5>
@@ -55,7 +57,7 @@
                                 </div>
                             </div>
                             <!-- Product actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                            <div class="card-footer p-2 pt-0 border-top-0 bg-transparent">
                                 <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Obter</a></div>
                             </div>
                         </div>  
